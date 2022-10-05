adf00a71348d968fe1468b19f78953aa428b7a6f
DuGuYifei <liuyifei97@163.com>
2022-10-05 19:35:39 +0200

    build my own api

---
+++ READMA.md
@@ -0,0 +1,5 @@
+# README
+
+Build my own api website. (netyicloudmusicapi.liuyifei.tech). The API respository(https://github.com/DuGuYifei/NeteaseCloudMusicApi)
+
+Use Vue to create a music player which can search/play music/mv, show comments from Netyi Cloud Music.
--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 /*
   1:歌曲搜索接口
-    请求地址:https://autumnfish.cn/search
+    请求地址:https://netyicloudmusicapi.liuyifei.tech/search
     请求方法:get
     请求参数:keywords(查询关键字)
     响应内容:歌曲搜索结果
   2:歌曲url获取接口
-    请求地址:https://autumnfish.cn/song/url
+    请求地址:https://netyicloudmusicapi.liuyifei.tech/song/url
     请求方法:get
     请求参数:id(歌曲id)
     响应内容:歌曲url地址
   3.歌曲详情获取
-    请求地址:https://autumnfish.cn/song/detail
+    请求地址:https://netyicloudmusicapi.liuyifei.tech/song/detail
     请求方法:get
     请求参数:ids(歌曲id)
     响应内容:歌曲详情(包括封面信息)
   4.热门评论获取
-    请求地址:https://autumnfish.cn/comment/hot?type=0
+    请求地址:https://netyicloudmusicapi.liuyifei.tech/comment/hot?type=0
     请求方法:get
     请求参数:id(歌曲id,地址中的type固定为0)
     响应内容:歌曲的热门评论
   5.mv地址获取
-    请求地址:https://autumnfish.cn/mv/url
+    请求地址:https://netyicloudmusicapi.liuyifei.tech/mv/url
     请求方法:get
     请求参数:id(mvid,为0表示没有mv)
     响应内容:mv的地址
@@ -45,9 +45,10 @@ var app = new Vue({
     methods: {
         searchMusic: function () {
             var that = this;
-            axios.get("https://autumnfish.cn/search?keywords=" + this.query)
+            axios.get("https://netyicloudmusicapi.liuyifei.tech/cloudsearch?keywords=" + this.query)
                 .then(
                     function (response) {
+						console.log(response);
                         that.musicList = response.data.result.songs;
                     },
                     function (err) { }
@@ -55,19 +56,19 @@ var app = new Vue({
         },
         playMusic(id) {
             var that = this;
-            axios.get("https://autumnfish.cn/song/url?id=" + id)
+            axios.get("https://netyicloudmusicapi.liuyifei.tech/song/url?id=" + id)
                 .then(function (response) {
                     that.musicUrl = response.data.data[0].url;
                 }, function (err) { });
-            axios.get("https://autumnfish.cn/song/detail?ids=" + id)
+            axios.get("https://netyicloudmusicapi.liuyifei.tech/song/detail?ids=" + id)
                 .then(function (response) {
                     that.musicCover = response.data.songs[0].al.picUrl;
                 }, function (err) { });
-            axios.get('https://autumnfish.cn/comment/hot?type=0&id=' + id)
+            axios.get('https://netyicloudmusicapi.liuyifei.tech/comment/hot?type=0&id=' + id)
                 .then(function (response) {
                     that.musicComment = response.data.hotComments;
                 }, function (err) { });
-            axios.get('https://autumnfish.cn//lyric?id=' + id)
+            axios.get('https://netyicloudmusicapi.liuyifei.tech/lyric?id=' + id)
                 .then(function(response){
                     //console.log(response.data.lrc.lyric);
                     let rows = response.data.lrc.lyric.split('\n');
@@ -142,7 +143,7 @@ var app = new Vue({
         playMV(mvid) {
             var that = this;
             this.isShow = true;
-            axios.get("https://autumnfish.cn/mv/url?id=" + mvid)
+            axios.get("https://netyicloudmusicapi.liuyifei.tech/mv/url?id=" + mvid)
                 .then(function (response) {
                     that.mvUrl = response.data.data.url;
                 }, function (err) { })
